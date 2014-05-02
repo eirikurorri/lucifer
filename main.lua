3921852a6f69aab7d7dcecfcdff0317ea8070fe2
Eirikur Orri Olafsson <eirikurorri@gmail.com>
2014-05-02 16:37:57 +0000

    inserted pink dude! O wow!

--- main.lua
+++ main.lua
@@ -1,15 +1,19 @@
 
 function love.load()
+	-- background
     background=love.graphics.newImage('gfx/cave.png')
     background2= love.graphics.newImage('gfx/cave.png')
     background1y = 0
     background2y = 490
     dir =  -32
+    -- lucifer's character
+    pinkdude = love.graphics.newImage('gfx/pinkdude.bmp')
 end
 
 function love.draw()
     love.graphics.draw(background, 0, background1y )
     love.graphics.draw(background2, 0, background2y )
+    love.graphics.draw(pinkdude, 450, 180)
 end
 
 function love.update( dt )
