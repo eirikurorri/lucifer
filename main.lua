a9540e2a10d629ee1cf6799a0228f66a0fe53ef7
GeneralDoddi <doddisti@gmail.com>
2014-05-03 01:13:24 +0000

    FPS meter added, and an attempt for background reset

--- main.lua
+++ main.lua
@@ -1,26 +1,31 @@
 
 function love.load()
 	-- background
-    background=love.graphics.newImage('gfx/cave.png')
-    background2= love.graphics.newImage('gfx/cave.png')
-    background1y = 0
-    background2y = 490
-    dir =  -32
+    background = love.graphics.newImage('gfx/cave.png')
+    background2 = love.graphics.newImage('gfx/cave.png')
+    backgroundy = 0
+    background2y = 440
+    dir =  -128
     -- lucifer's character
     pinkdude = love.graphics.newImage('gfx/pinkdude.bmp')
     dudeSpeed = 600
-    dudeX = 400
+    dudeX = 200
 end
 
 function love.draw()
-    love.graphics.draw(background, 0, background1y )
-    love.graphics.rotate(1)
-    love.graphics.draw(background2, 0, background2y )
+    -- background drawing
+    --love.graphics.rotate(1.57079633)
+    love.graphics.draw(background, 160, backgroundy )
+    love.graphics.draw(background2, 160, background2y )
+    -- our character drawn
     love.graphics.draw(pinkdude, dudeX, 180)
+    -- FPS meter
+    love.graphics.print("FPS: "..love.timer.getFPS() .. '\nMem(kB): ' .. math.floor(collectgarbage("count")), 680, 20)
+    
 end
 
 function love.update( dt )
-    background1y = background1y + dir * dt
+    backgroundy = backgroundy + dir * dt
     background2y = background2y + dir * dt
     if (love.keyboard.isDown("left")) then 
     	dudeX = dudeX - dudeSpeed * dt
@@ -28,4 +33,9 @@ function love.update( dt )
     if (love.keyboard.isDown("right")) then 
     	dudeX = dudeX + dudeSpeed * dt
     end
+
+    --if backgroundy == 490 then
+    --    love.graphics.print("DERPTASTIC", 680, 80)
+    --end
+
 end
