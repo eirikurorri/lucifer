14f28130c49d83db700b7b7c50f929564497bfe9
GeneralDoddi <doddisti@gmail.com>
2014-05-05 22:46:25 +0000

    trying out reverse mode

--- main.lua
+++ main.lua
@@ -15,12 +15,15 @@ local Camera = require "hump.camera"
 local ourHero = require "hero"
 local collider
 local allSolidTiles
+local distanceGoal
+local distance
 
 function love.load()
 
 	-- Tiled stuff
 	--map = loader.load("testmap.tmx")
-    
+    map = loader.load("tilemap.tmx")
+    map:setDrawRange(0,0,640,32000)
 	-- End Tiled stuff
     camY = 0
     cam = Camera(400,0)
@@ -29,33 +32,19 @@ function love.load()
 	-- load HardonCollider, set callback to on_collide and size of 100
     collider = HC(100, on_collide)
     -- find all the tiles that we can collide with
-    --allSolidTiles = findSolidTiles(map)
+    allSolidTiles = findSolidTiles(map)
     -- set up the hero object, set him to position 32, 32
     ourHero.setupHero(32,32, collider)
 
-
+    reached_bottom = false
 	-- background
     background.loadBackground()
-    -- lucifer's character
-    -- player.loadPlayer()
-    -- ledge obstacle
-    ledge = love.graphics.newImage('gfx/ledge.bmp')
-    -- soul dude
-    soul = love.graphics.newImage('gfx/souldude.bmp')
-    soulmovementX = 0 -- want to have soul dude float back and forth a little bit
-    
-    -- lucifer's character
-    --player.loadPlayer()
-
-    ledgey = 0
-    souly = 0
 
     --distance monitor and goal
-    distanceGoal = 100000
+    distanceGoal = 32000
     distance = 0
     -- Tiled stuff
-	map = loader.load("testmap.tmx")
-    map:setDrawRange(0,0,640,32000)
+	
 	-- End Tiled stuff
 
 
@@ -71,31 +60,23 @@ function love.draw()
     -- background drawing
     background.drawBackground()
     background.debugBackground()
-    -- player avatar drawn
-    -- player.drawPlayer()
-    -- ledge drawing
-    love.graphics.draw(ledge, -20, ledgey + bgheight )
-    -- soul dude drawn
-    love.graphics.draw(soul, 550, 200 + souly + bgheight )
+
+    
     -- FPS meter and memory counter
     love.graphics.print("FPS: "..love.timer.getFPS() .. '\nMem(kB): ' .. math.floor(collectgarbage("count")), 680, 20)
 
 	-- Tiled stuff
 	cam:draw(drawCamera)
 	-- end Tiled stuff
-	
+	if distance >= distanceGoal then
+        reached_bottom = true
+        love.graphics.print("DOWN", 680, 140)
+    elseif distance <= 0 then
+        reached_bottom = false
+        love.graphics.print("UP", 680, 160)
+    end
 
     -- scrolling speed for ledge and soul
-    ledgey = ledgey - 6
-    souly = souly - 6
-   
-    if ledgey <= -2000 then
-        ledgey = 0
-    end
-
-    if souly <= -2000 then
-        souly = 0
-    end
 
 end
 
@@ -106,32 +87,37 @@ function drawCamera()
 
 end
 
-function findSolidTiles(map)
-	local collidable_tiles = {}
-
-    for x, y, tile in map("ground"):iterate() do
-		love.graphics.print(string.format("Tile at (%d,%d) has an id of %d", x, y, tile.id),10,10)
-		--if tile.properties.solid then
-			local ctile = collider:addRectangle((x)*32,(y)*32,32,32)
-            ctile.type = "tile"
-            collider:addToGroup("tiles", ctile)
-            collider:setPassive(ctile)
-            table.insert(collidable_tiles, ctile)
-		--end
-	end
-
-    return collidable_tiles
-end
-
-
-
 function love.update(dt)
     -- player events handled
     --player.updatePlayer(dt) 
     ourHero.handleInput(dt)
     ourHero.updateHero(dt)
     collider:update(dt) 
-    camY = camY + 4
+
+    if reached_bottom == false then
+        distance = distance + 4
+        camY = camY + 4
+    else
+        distance = distance - 4
+        camY = camY - 4
+    end
     cam:lookAt(400,camY)   
 
+end
+
+function findSolidTiles(map)
+    local collidable_tiles = {}
+
+    for x, y, tile in map("ground"):iterate() do
+        love.graphics.print(string.format("Tile at (%d,%d) has an id of %d", x, y, tile.id),10,10)
+        --if tile.properties.solid then
+            local ctile = collider:addRectangle((x)*32,(y)*32,32,32)
+            ctile.type = "tile"
+            collider:addToGroup("tiles", ctile)
+            collider:setPassive(ctile)
+            table.insert(collidable_tiles, ctile)
+        --end
+    end
+
+    return collidable_tiles
 end
