e779a724b191466e3f12350c8415f6182a54dd27
GeneralDoddi <doddisti@gmail.com>
2014-05-08 18:44:23 +0000

    offset changes

--- hero.lua
+++ hero.lua
@@ -25,19 +25,18 @@ function hero.herocoords()
     return math.floor(heroy)
 end
 
-function hero.updateHero(dt,cam,speed,reached_bottom)
+function hero.updateHero(dt,cam,speed,reached_bottom,distanceGoal,cameraoffset)
 	-- apply a downward force to the hero (=gravity)
 	camx,heroy = ourHero:center()
-    print(reached_bottom)
     if reached_bottom == false then
-        if heroy < 2500 then
+        if heroy < distanceGoal - cameraoffset then
             ourHero:move(0,dt*speed) -- collider.move 
             cam:lookAt(400,heroy+offset+dt*speed)
         else
             ourHero:move(0,dt*speed)
         end
     elseif reached_bottom == true then
-        if heroy < 700 then
+        if heroy < cameraoffset then
             ourHero:move(0,-dt*speed)
             
         else
@@ -73,13 +72,13 @@ function hero.collideHeroWithTile(dt, shape_a, shape_b, mtv_x, mtv_y)
    elseif shape_a == ourHero and shape_b.type == "soul" then
         map.layers["souls"]["objects"][shape_b.key]["visible"] = false
         -- map.layers["soulTiles"]["objects"][shape_b.key]["visible"] = false
-        -- collider:remove(shape_b)
+         collider:remove(shape_b)
         scorecounter()
         return
     elseif shape_b == ourHero and shape_a.type == "soul" then
         map.layers["souls"]["objects"][shape_a.key]["visible"] = false
         --map.layers["soulTiles"]["objects"][shape_a.key]["visible"] = false
-        -- collider:remove(shape_a)
+         collider:remove(shape_a)
         scorecounter()
         return
    else
--- main.lua
+++ main.lua
@@ -24,6 +24,7 @@ local scorecount
 local gamestate = "menu"
 local herospeed = 0
 local speedmargin = 0.1
+local cameraoffset = 700
 
 love.window.setMode(1200, 800)
 
@@ -31,8 +32,8 @@ function love.load()
 
 	-- Tiled stuff
 	--map = loader.load("testmap.tmx")
-    map = loader.load("testmap2.tmx")
-    map:setDrawRange(0,0,800,32000)
+    map = loader.load("derpmap.tmx")
+    map:setDrawRange(120,0,960,32000)
     -- map("object").visible = false -- makes object map invisible
 	-- End Tiled stuff
     camY = 0
@@ -53,7 +54,7 @@ function love.load()
     background.loadBackground()
 
     --distance monitor and goal
-    distanceGoal = 3200
+    distanceGoal = 32000
     distance = 0
     -- Tiled stuff
     -- speedometer, use for different speeds
